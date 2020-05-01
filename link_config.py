84b30e15d81db097f6f2edf7d1cba1163dc13ae1
Minh Le <minh.le@robinhood.com>
2020-05-01 13:58:21 -0700

    vim reorg

---
+++ link_config.py
@@ -0,0 +1,28 @@
+ os
+import shutil
+
+
+REPO_DIR = ""
+HOME_DIR = ""
+
+
+def link_file(fpath):
+    pass
+
+
+VIM_FILES = [
+    ".vim/init.vim"
+    ".vim/init.vimrc"
+    ".vim/basic.vimrc"
+    ".vim/keys.vimrc"
+    ".vimrc"
+]
+ZSH_FILES = [
+    #zshrc
+    #theme
+    #ohmyzsh
+    #alias files
+]
+TMUX_FILES = []
+
+
