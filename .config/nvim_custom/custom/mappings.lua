134f1716a73888059388c9d0ffc77f15b45eec1d
Minh Le <minh.d.le27@gmail.com>
2024-04-20 09:18:40 -0700

    hi

--- .config/nvim_custom/custom/mappings.lua
+++ .config/nvim_custom/custom/mappings.lua
@@ -2,116 +2,117 @@
 local M = {}
 
 M.general = {
-	n = {
-		[";"] = { ":", "enter command mode", opts = { nowait = true } },
-		["fn"] = { ":let @+ = expand('%')<cr>", "Copy file path" },
-		["cn"] = {
-			":cn<cr>",
-			":cn",
-		},
-		["cN"] = {
-			":cn<cr>",
-			":cN<cr>",
-		},
+  n = {
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["fn"] = { ":let @+ = expand('%')<cr>", "Copy file path" },
+    ["cn"] = {
+      ":cn<cr>",
+      ":cn",
+    },
+    ["cN"] = {
+      ":cn<cr>",
+      ":cN<cr>",
+    },
     ["fm"] = {
       function()
         vim.lsp.buf.format { async = true }
       end,
       "LSP formatting",
     },
-	},
+  },
 }
 
 -- more keybinds!
 M.comment = {
-	plugin = true,
-	-- toggle comment in both modes
-	n = {
-		["<leader>cc"] = {
-			function()
-				require("Comment.api").toggle.linewise.current()
-			end,
-			"toggle comment",
-		},
-	},
-	v = {
-		["<leader>cc"] = {
-			"<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
-			"toggle comment",
-		},
-	},
+  plugin = true,
+  -- toggle comment in both modes
+  n = {
+    ["<leader>cc"] = {
+      function()
+        require("Comment.api").toggle.linewise.current()
+      end,
+      "toggle comment",
+    },
+  },
+  v = {
+    ["<leader>cc"] = {
+      "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
+      "toggle comment",
+    },
+  },
 }
 
 M.fzf = {
-	n = {
-		["<C-s>"] = {
-			function()
-				require("fzf-lua").grep_project()
-			end,
-			"fzf search lines",
-		},
-		["<C-p>"] = {
-			function()
-				require("fzf-lua").files()
-			end,
-			"fzf search files",
-		},
-		["<C-f>"] = {
-			function()
-				require("fzf-lua").grep_curbuf()
-			end,
-			"fzf search buffer",
-		},
-		["<C-b>"] = {
-			function()
-				require("fzf-lua").buffers()
-			end,
-			"fzf search buffer",
-		},
-		-- ["<C-g>"] = {
-		-- 	function()
-		-- 		require("fzf-lua").args()
-		-- 	end,
-		-- 	"fzf search args",
-		-- },
-		-- ["<C-q>"] = {
-		--   "<ESC><cmd>lua require('fzf-lua').lsp_workspace_diagnostics()<CR>",
-		--   "fzf search lsp diagnostics",
-		-- },
-		-- ["<C-h>"] = {
-		--   "<ESC><cmd>lua require('fzf-lua').command_history()<CR>",
-		--   "fzf search history"
-		-- },
-	},
+  n = {
+    ["<C-s>"] = {
+      function()
+        require("fzf-lua").grep_project()
+      end,
+      "fzf search lines",
+    },
+    ["<C-p>"] = {
+      function()
+        require("fzf-lua").files()
+      end,
+      "fzf search files",
+    },
+    ["<C-f>"] = {
+      function()
+        require("fzf-lua").grep_curbuf()
+      end,
+      "fzf search buffer",
+    },
+    ["<C-b>"] = {
+      function()
+        require("fzf-lua").buffers()
+      end,
+      "fzf search buffer",
+    },
+    -- ["<C-g>"] = {
+    -- 	function()
+    -- 		require("fzf-lua").args()
+    -- 	end,
+    -- 	"fzf search args",
+    -- },
+    -- ["<C-q>"] = {
+    --   "<ESC><cmd>lua require('fzf-lua').lsp_workspace_diagnostics()<CR>",
+    --   "fzf search lsp diagnostics",
+    -- },
+    -- ["<C-h>"] = {
+    --   "<ESC><cmd>lua require('fzf-lua').command_history()<CR>",
+    --   "fzf search history"
+    -- },
+  },
 }
 
 M.lspconfig = {
-	plugin = true,
-	n = {
-		["<leader>q"] = {
-			function()
-				vim.diagnostic.setqflist()
-			end,
-			"diagnostic setloclist",
-		},
-	},
+  plugin = true,
+  n = {
+    ["<leader>q"] = {
+      function()
+        vim.diagnostic.setqflist()
+      end,
+      "diagnostic setloclist",
+    },
+  },
 }
 
-M.pytest = {
-	n = {
-		-- ["pta"] = {
-		--   function()
-		--     vim.cmd ':exe "!tmux send -t 2 \'pytest \'" . "  Enter"'
-		--   end,
-		--   "run pytest on current file in buffer",
-		-- },
-		["rp"] = {
-			function()
-				vim.cmd(":silent exe \"!tmux send -t 2 'pytest -n0 -s \" . expand('%') .  \" ' Enter\"")
-			end,
-			"run pytest on current file in buffer",
-		},
-	},
+M.tmux = {
+  n = {
+    ["<leader>rr"] = {
+      function()
+        pane_number = 2
+        vim.fn.system("tmux send-keys -t " .. pane_number .. " C-p")
+        vim.fn.system("tmux send-keys -t " .. pane_number .. " Enter")
+      end
+    },
+    ["rp"] = {
+      function()
+        vim.cmd(":silent exe \"!tmux send -t 2 'pytest -s \" . expand('%') .  \" ' Enter\"")
+      end,
+      "run pytest on current file in buffer",
+    },
+  },
 }
 
 -- M.codium = {
@@ -126,13 +127,13 @@ M.pytest = {
 -- }
 
 M.nvim_treesitter_context = {
-	n = {
-		["[c"] = {
-			function()
-				require("treesitter-context").go_to_context()
-			end,
-		},
-	},
+  n = {
+    ["[c"] = {
+      function()
+        require("treesitter-context").go_to_context()
+      end,
+    },
+  },
 }
 
 return M
