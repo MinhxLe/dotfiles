e6cdd554092bca4260f9b61c0d624bc915cb0154
Minh Le <minh@usebeam.co>
2024-04-22 09:51:35 -0700

    hi

--- .config/nvim_custom/custom/configs/conform.lua
+++ .config/nvim_custom/custom/configs/conform.lua
@@ -1,16 +1,26 @@
+--type conform.options
 local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    python = { "ruff_format" },
-    css = { "prettier" },
-    html = { "prettier" },
-  },
+	lsp_fallback = true,
 
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		typescript = { "eslint_d" },
+		typescriptreact = { "eslint_d" },
+		-- javascript = { "prettier" },
+		css = {},
+		html = {},
+		python = { "ruff_format" },
+		sh = { "shfmt" },
+	},
+
+	-- adding same formatter for multiple filetypes can look too much work for some
+	-- instead of the above code you could just use a loop! the config is just a table after all!
+
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 200,
+		lsp_fallback = false,
+	},
 }
 
 require("conform").setup(options)
